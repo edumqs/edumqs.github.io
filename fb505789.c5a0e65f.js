da78bf5afd4833cd6105fac4b4a3a1647c2dc2af
Eduardo Marques <edumqs@gmail.com>
2021-06-15 21:59:53 +0200

    status page update

---
+++ fb505789.c5a0e65f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[63],{133:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"beginning","name":"beginning","count":1,"permalink":"/blog/tags/beginning"}')}}]);
